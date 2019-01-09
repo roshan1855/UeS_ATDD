b6b1ba2299ade411cb3b2d9e214ea0fb9c080a40
dsuribab <dsuribab@LH7U05CG6422PFF.ms.ds.uhc.com>
2019-01-09 15:28:39 +0530

    Data Sheet commit

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -14,7 +14,7 @@ import cucumber.api.junit.Cucumber;
 		// features = {"src/main/resources/features" },
 		format = { "json:target/cucumber.json" },
 
-		tags = { "@Display_Class1_Class2_Plans_WithOutSalAndDep"
+		tags = { "@COBRA"
 		// + ",@Regression"
 		// + ",@July_Regression"
 
